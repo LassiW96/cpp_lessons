ae3b5fa4118c63bf35df6729531f52e6dd1f49a0
lassi <lasithagayan9596@gmail.com>
2025-09-03 17:27:26 -0500

    OOP8 - An app to create circular wave patterns

---
+++ OOP8/WaveformGenerator.h
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Header file for the superposition, when there are more than one waves
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# pragma once
+# include "Wave.h"
+# include <vector>
+
+class WaveformGenerator {
+private: 
+    std::vector<Wave> waves; // Vector to hold multiple wave objects of the class Wave
+
+public:
+    // Function to store wave objects in the "waves" vector
+    void AddWaves(const Wave& wave) {
+        waves.push_back(wave);
+    }
+
+    // Main superposition function
+    // duration - time duration of the signal
+    // TotSamples - how many samples (points you calculate) you want to divide the wave
+    //              more samples - smoother curve
+    std::vector<double> Generate(double duration, double TotSamples) {
+        std::vector<double> Data(TotSamples, 0.0);              // Data vector to store sampled data points, all starts with 0.0
+
+        for (int i = 0; i < TotSamples; i++) {
+            // Calculating the time stamp
+            double t = (duration * i) / TotSamples;
+
+            // Get the value at the time stamp, add it and create the final Data vector
+            for (const auto& w : waves) {
+                Data[i] += w.GetValue(t);
+            }
+        }
+
+        return Data;
+    }
+
+};
